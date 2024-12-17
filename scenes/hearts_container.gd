3f40bf945ae2e9257ae05eb9b1f94ea5b5863c15
Yvan Brito <yvanbrito96@gmail.comm>
2024-12-16 21:22:25 -0300

    adding sword animations

--- scenes/hearts_container.gd
+++ scenes/hearts_container.gd
@@ -2,15 +2,14 @@ extends HBoxContainer
 
 const HEART_GUI = preload("res://prefabs/UI_elem/heart_gui.tscn")
 
-func setMaxHearts(max: int):
-	for i in range(max):
+func setMaxHearts(maxHearts: int):
+	for i in range(maxHearts):
 		var heart = HEART_GUI.instantiate()
 		add_child(heart)
 
 func updateHearts(currentHealth):
 	var hearts = get_children()
 	for i in range(hearts.size()):
-		print(i)
 		if currentHealth >= (i+1)*2:
 			hearts[i].update(0)
 		elif currentHealth == (i+1)*2-1:
--- scripts/player.gd
+++ scripts/player.gd
@@ -5,32 +5,56 @@ const SPEED = 100.0
 var last_direction: Vector2
 var maxHealth: int = 6
 var currentHealth: int = 6: set = _set_current_health, get = _get_current_health
+var isAttacking: bool = false
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var sword_animated_sprite: AnimatedSprite2D = $SwordAnimatedSprite
 
-func _set_current_health(new_value: float) -> void:
+func _set_current_health(new_value: int) -> void:
 	if new_value > maxHealth:
 		currentHealth = maxHealth
 	else:
 		currentHealth = new_value
 
-func _get_current_health() -> float:
+func _get_current_health() -> int:
 	return currentHealth
 
-func _physics_process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	process_inputs()
 	process_anims()
-
+	
+func _physics_process(_delta: float) -> void:
 	move_and_slide()
 
 func process_inputs() -> void:
 	var horizontal_dir := Input.get_axis("ui_left", "ui_right")
 	var vertical_dir := Input.get_axis("ui_up", "ui_down")
 	
-	velocity = Vector2(horizontal_dir, vertical_dir).normalized() * SPEED
+	if Input.is_action_pressed("action_tool"):
+		isAttacking = true
+	
+	if isAttacking:
+		velocity = Vector2.ZERO
+	else:
+		velocity = Vector2(horizontal_dir, vertical_dir).normalized() * SPEED
 
 func process_anims() -> void:
-	if velocity != Vector2.ZERO:
+	if isAttacking:
+		sword_animated_sprite.visible = true
+		if last_direction.y > 0:
+			sword_animated_sprite.position = Vector2(0, -4)
+			animated_sprite_2d.play("attack1_down")
+			sword_animated_sprite.play("attack1_down")
+		elif last_direction.y < 0:
+			sword_animated_sprite.position = Vector2(0, -8)
+			animated_sprite_2d.play("attack1_up")
+			sword_animated_sprite.play("attack1_up")
+		elif last_direction.x != 0:
+			sword_animated_sprite.position = Vector2(0, -8)
+			animated_sprite_2d.flip_h = last_direction.x < 0
+			animated_sprite_2d.play("attack1_hor")
+			sword_animated_sprite.play("attack1_hor")
+	elif velocity != Vector2.ZERO:
 		last_direction = velocity
 		if abs(velocity.y) > abs(velocity.x):
 			if velocity.y > 0:
@@ -40,6 +64,7 @@ func process_anims() -> void:
 		elif velocity.x != 0:
 			animated_sprite_2d.play("running_hor")
 			animated_sprite_2d.flip_h = velocity.x < 0
+			sword_animated_sprite.flip_h = velocity.x < 0
 	else:
 		if last_direction.y > 0:
 			animated_sprite_2d.play("idle_down")
@@ -48,3 +73,9 @@ func process_anims() -> void:
 		elif last_direction.x != 0:
 			animated_sprite_2d.flip_h = last_direction.x < 0
 			animated_sprite_2d.play("idle_hor")
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if animated_sprite_2d.animation.begins_with("attack"):
+		sword_animated_sprite.visible = false
+		isAttacking = false
